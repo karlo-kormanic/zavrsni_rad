c8d8d5c3ba087696595c11f900056e2dfa023ffe
karlo-kormanic <karlo.kormanic9@gmail.com>
2025-09-02 11:21:46 +0200

    Made login text more visible, removed unintentional scroll on player response page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,7 +42,7 @@ function GoogleLoginButton() {
   return (
     <button
       onClick={handleLogin}
-      className="w-full bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition"
+      className="w-full bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition"
     >
       Sign in with Google
     </button>
